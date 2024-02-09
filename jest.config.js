26994ab2f4497a06b6e4b670ed30632cfab158bc
Marcio Viveiros <Marcio.Viveiros@sapo.pt>
2024-02-09 10:18:44 +0000

    add jest.config.js enables defining timeout

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testTimeout: 30000, // Timeout of 10000 ms for all tests
+    // Add other configurations as needed
+  };
